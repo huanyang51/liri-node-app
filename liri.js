9badb971c6a0dad902a02b834f54a67c542d55c9
hycoding51 <yanghuan.bnu@gmail.com>
2020-01-18 14:13:26 -0500

    feat: add feature to search for events for artists with Bandsintown API

--- liri.js
+++ liri.js
@@ -1,4 +1,51 @@
 require("dotenv").config();
+const fs = require("fs");
+const axios = require("axios");
+const moment = require("moment");
 var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
+// take in commands
+const [_, __, operation] = process.argv;
+var name = process.argv.slice(3).join(" ");
+switch (operation) {
+  case "concert-this":
+    axios
+      .get(
+        "https://rest.bandsintown.com/artists/" +
+          name +
+          "/events?app_id=codingbootcamp"
+      )
+      .then(function(response) {
+        var events = response.data;
+        var numOfEvents = events.length;
+        if (numOfEvents === 0) {
+          console.log(`No events found for ${name}.`);
+        } else {
+          for (var i = 0; i < numOfEvents; i++) {
+            var venue = events[i].venue;
+            var time = moment(events[i].datetime.split("T")[0]).format(
+              "MM/DD/YYYY"
+            );
+            console.log(
+              `Event ${[i + 1]}:\nName of the Venue: ${
+                venue.name
+              }\nThe Location of the Venue: ${venue.city}, ${venue.region}, ${
+                venue.country
+              }\nThe Date of the Event: ${time} `
+            );
+          }
+        }
+      });
+    break;
+  case "spotify-this-song":
+    break;
+  case "movie-this":
+    break;
+  case "do-what-it-says":
+    break;
+  default:
+    console.log(
+      `Unsupported command. Please try "concert-this", "spotify-this-song", "movie-this" or "do-what-it-says".`
+    );
+}
