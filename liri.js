f635c233d68993e8e42484d22cac5b9e130305e1
hycoding51 <yanghuan.bnu@gmail.com>
2020-01-18 20:55:09 -0500

    refactor: put code for searching into functions

--- liri.js
+++ liri.js
@@ -15,74 +15,85 @@ function toTitleCase(str) {
   });
 }
 name = toTitleCase(name);
+// functions to search concerts, songs and movies
+function toSearchConcerts(name) {
+  axios
+    .get(
+      `https://rest.bandsintown.com/artists/${name}/events?app_id=${keys.bandsId}`
+    )
+    .then(function(response) {
+      var events = response.data;
+      var numOfEvents = events.length;
+      if (numOfEvents === 0) {
+        console.log(`No events found for ${name}.`);
+      } else {
+        for (var i = 0; i < numOfEvents; i++) {
+          var venue = events[i].venue;
+          var time = moment(events[i].datetime.split("T")[0]).format(
+            "MM/DD/YYYY"
+          );
+          console.log(
+            `Event ${[i + 1]}:\nName of the Venue: ${
+              venue.name
+            }\nThe Location of the Venue: ${venue.city}, ${venue.region}, ${
+              venue.country
+            }\nThe Date of the Event: ${time} `
+          );
+        }
+      }
+    });
+}
+function toSearchSongs(name) {
+  var songsFromRes = [];
+  if (name === "") {
+    name = "The Sign";
+    //   if user didn't enter the name of a song, the app will call api to get the info of the song "The Sign"
+  }
+  spotify
+    .search({ type: "track", query: name })
+    .then(function(response) {
+      for (var i = 0; i < 20; i++) {
+        songsFromRes.push(response.tracks.items[i].name);
+      }
+      var songIdx = songsFromRes.indexOf(name);
+      var song = response.tracks.items[songIdx];
+      console.log(
+        `Artist(s): ${song.artists[0].name}\nThe song's name: ${song.name}\nPreview link: ${song.preview_url}\nThe album that the song is from: ${song.album.name}`
+      );
+    })
+    .catch(function(err) {
+      console.log("Sorry, it didn't work. Please try something else.");
+    });
+}
+function toSearchMovies(name) {
+  if (name === "") {
+    name = "Mr. Nobody";
+  }
+  axios
+    .get(`http://www.omdbapi.com/?t=${name}&apikey=${keys.movieId}`)
+    .then(function(response) {
+      var movie = response.data;
+      var releasedYear = movie.Released.split(" ")[2];
+      console.log(movie);
+      console.log(
+        `The title of the movie: ${movie.Title}\nReleased Year: ${releasedYear}\nIMDB Rating: ${movie.Ratings[0].Value}\nRotten Tomatoes Rating: ${movie.Ratings[1].Value}\nProduced Countries: ${movie.Country}\nLanguage: ${movie.Language}`
+      );
+      console.log(`Plot: ${movie.Plot}\nActors: ${movie.Actors}`);
+    })
+    .catch(function(error) {
+      console.log("Sorry, it didn't work. Please try something else.");
+    });
+}
+// take in commands from node
 switch (operation) {
   case "concert-this":
-    axios
-      .get(
-        `https://rest.bandsintown.com/artists/${name}/events?app_id=${keys.bandsId}`
-      )
-      .then(function(response) {
-        var events = response.data;
-        var numOfEvents = events.length;
-        if (numOfEvents === 0) {
-          console.log(`No events found for ${name}.`);
-        } else {
-          for (var i = 0; i < numOfEvents; i++) {
-            var venue = events[i].venue;
-            var time = moment(events[i].datetime.split("T")[0]).format(
-              "MM/DD/YYYY"
-            );
-            console.log(
-              `Event ${[i + 1]}:\nName of the Venue: ${
-                venue.name
-              }\nThe Location of the Venue: ${venue.city}, ${venue.region}, ${
-                venue.country
-              }\nThe Date of the Event: ${time} `
-            );
-          }
-        }
-      });
+    toSearchConcerts(name);
     break;
   case "spotify-this-song":
-    var songsFromRes = [];
-    if (name === "") {
-      name = "The Sign";
-      //   if user didn't enter the name of a song, the app will call api to get the info of the song "The Sign"
-    }
-    spotify
-      .search({ type: "track", query: name })
-      .then(function(response) {
-        for (var i = 0; i < 20; i++) {
-          songsFromRes.push(response.tracks.items[i].name);
-        }
-        var songIdx = songsFromRes.indexOf(name);
-        var song = response.tracks.items[songIdx];
-        console.log(
-          `Artist(s): ${song.artists[0].name}\nThe song's name: ${song.name}\nPreview link: ${song.preview_url}\nThe album that the song is from: ${song.album.name}`
-        );
-      })
-      .catch(function(err) {
-        console.log("Sorry, it didn't work. Please try something else.");
-      });
+    toSearchSongs(name);
     break;
   case "movie-this":
-    if (name === "") {
-      name = "Mr. Nobody";
-    }
-    axios
-      .get(`http://www.omdbapi.com/?t=${name}&apikey=${keys.movieId}`)
-      .then(function(response) {
-        var movie = response.data;
-        var releasedYear = movie.Released.split(" ")[2];
-        console.log(movie);
-        console.log(
-          `The title of the movie: ${movie.Title}\nReleased Year: ${releasedYear}\nIMDB Rating: ${movie.Ratings[0].Value}\nRotten Tomatoes Rating: ${movie.Ratings[1].Value}\nProduced Countries: ${movie.Country}\nLanguage: ${movie.Language}`
-        );
-        console.log(`Plot: ${movie.Plot}\nActors: ${movie.Actors}`);
-      })
-      .catch(function(error) {
-        console.log("Sorry, it didn't work. Please try something else.");
-      });
+    toSearchMovies(name);
     break;
   case "do-what-it-says":
     break;
